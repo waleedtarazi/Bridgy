0b50c05bddb9a45d038e11eedf6ff902997b0b36
waleed tarazi <81421459+waleedtarazi@users.noreply.github.com>
2025-05-07 16:26:24 +0300

    Update main.py
    update the heaer thing

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def check_house(request: Request):
     print(headers)
     print(" headers TYPE ")
     print( type(headers) )
-    number_of_adults = headers.get('number_of_adults')
+    number_of_adults = headers.get('number-of-adults')
     logging.info(f"API received with number_of_adults = {number_of_adults}")
     if int(number_of_adults) > 3:
         return {"can_have_house": False}
