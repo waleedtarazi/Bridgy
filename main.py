a11db543880329563b0ccb9f7b73e6e12aacf429
waleed tarazi <81421459+waleedtarazi@users.noreply.github.com>
2025-05-07 16:16:57 +0300

    Update main.py
    debugging header problem

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI, Header, HTTPException
+from fastapi import FastAPI, Header, HTTPException, Request
 import logging
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
 @app.get("/check-house")
-def check_house(number_of_adults: str = Header(...)):
-    print(f"Received number_of_adults: {number_of_adults}")
+def check_house(request: Request):
+    headers = request.headers
+    print(' -- > the header <--- ')
+    print(header)
+    print(" headers TYPE ")
+    print( type(header) )
+    number_of_adults = header.get('number_of_adults')
     logging.info(f"API received with number_of_adults = {number_of_adults}")
     if int(number_of_adults) > 3:
         return {"can_have_house": False}
