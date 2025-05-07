feabb8e9f8006079b40152fc86216defbc408741
waleed tarazi <81421459+waleedtarazi@users.noreply.github.com>
2025-05-07 16:21:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = FastAPI()
 def check_house(request: Request):
     headers = request.headers
     print(' -- > the header <--- ')
-    print(header)
+    print(headers)
     print(" headers TYPE ")
-    print( type(header) )
-    number_of_adults = header.get('number_of_adults')
+    print( type(headers) )
+    number_of_adults = headers.get('number_of_adults')
     logging.info(f"API received with number_of_adults = {number_of_adults}")
     if int(number_of_adults) > 3:
         return {"can_have_house": False}
