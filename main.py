0d456af858a9ccd8a737db9f305593a28e39127d
waleed tarazi <81421459+waleedtarazi@users.noreply.github.com>
2025-05-07 15:51:06 +0300

    Update main.py
    add logging

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, Header, HTTPException
+import logging
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
 @app.get("/check-house")
 def check_house(number_of_adults: int = Header(...)):
+    print(f"Received number_of_adults: {number_of_adults}")
+    logging.info(f"API received with number_of_adults = {number_of_adults}")
     if number_of_adults > 3:
         return {"can_have_house": False}
     return {"can_have_house": True}
